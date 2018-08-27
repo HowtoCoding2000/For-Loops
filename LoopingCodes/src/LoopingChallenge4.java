cabaefb4cf8842b4d4dc9937f58ff7eeca77b7c4
calvin.farrell <calvin.farrell@MU-C1-12.mullen.pvt.k12.co.us>
2018-08-27 09:02:34 -0600

    adding Looping Challenges 3-5

---
+++ LoopingCodes/src/LoopingChallenge4.java
@@ -0,0 +1,12 @@
+
+public class LoopingChallenge4
+	{
+		public static void main(String[] args)
+			{
+				for (int i = 10; i > 0; i--)
+					{
+						System.out.println(i);
+					}
+				System.out.println("Liftoff!");
+			}
+	}
