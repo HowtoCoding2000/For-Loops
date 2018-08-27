add925a27b2d21b31ff55b790e876ca93c76de52
calvin.farrell <calvin.farrell@MU-C1-12.mullen.pvt.k12.co.us>
2018-08-27 08:57:42 -0600

    initialCommit

--- LoopingCodes/src/LoopingChallenge2.java
+++ LoopingCodes/src/LoopingChallenge2.java
@@ -3,6 +3,9 @@ public class LoopingChallenge2
 	{
 		public static void main(String[] args)
 			{
-				for (int i )
+				for (int i = 2; i <= 10; i = i + 2)
+					{
+						System.out.println(i + ") I love Mullen!");
+					}
 			}
 	}
