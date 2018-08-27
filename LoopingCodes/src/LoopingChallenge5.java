96e86ca74eb3edb6633a0170d50eb7f22c33a6d6
calvin.farrell <calvin.farrell@MU-C1-12.mullen.pvt.k12.co.us>
2018-08-27 09:10:07 -0600

    added Looping Challenge 6 and changed 5

--- LoopingCodes/src/LoopingChallenge5.java
+++ LoopingCodes/src/LoopingChallenge5.java
@@ -3,6 +3,11 @@ public class LoopingChallenge5
 	{
 		public static void main(String[] args)
 			{
+				int sumOfMultiplesOfFive = 0;
 				for (int i = 5; i <= 100; i = i + 5)
+					{
+						sumOfMultiplesOfFive = sumOfMultiplesOfFive + i;
+					}
+				System.out.println(sumOfMultiplesOfFive);
 			}
 	}
