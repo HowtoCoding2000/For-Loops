94e50a2e08d32e8e6f6f02d203454836ab736cec
calvin.farrell <calvin.farrell@MU-C1-12.mullen.pvt.k12.co.us>
2018-08-29 10:18:14 -0600

    finished challenge 8

--- LoopingCodes/src/LoopingChallenge8.java
+++ LoopingCodes/src/LoopingChallenge8.java
@@ -1,4 +1,13 @@
 
-public class LoopingChallenge8 {
-
+public class LoopingChallenge8 
+{
+	public static void main(String[] args)
+		{
+			System.out.println("*********");
+			for (int i = 1; i <= 9; i++)
+				{
+					System.out.println("*       *");
+				}
+			System.out.println("*********");
+		}
 }
